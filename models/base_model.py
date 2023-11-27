d1ff015678640842d15c8e1a8ab4cbd0db02745e
MicaViera <vieranoelia98@gmail.com>
2023-11-27 00:44:30 +0000

    Fixing base model class.

--- models/base_model.py
+++ models/base_model.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
-import uuid
+from uuid import uuid4
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, DateTime
 
+
 Base = declarative_base()
+
 
 class BaseModel:
     """A base class for all hbnb models"""
 
     id = Column(
         String(60),
-        unique=True,
         nullable=False,
         primary_key=True
     )
@@ -31,16 +32,17 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-        else:
+
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        if kwargs:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
+            if '__class__' in kwargs.keys():
+                del kwargs['__class__']
             self.__dict__.update(kwargs)
 
     def __str__(self):
