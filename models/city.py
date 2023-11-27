b62626646704d8ea05512ca4e9693b563e5f1892
MicaViera <vieranoelia98@gmail.com>
2023-11-27 01:49:25 +0000

    Restoring city class.

--- models/city.py
+++ models/city.py
@@ -1,26 +1,9 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Foreignkey
-from sqlalchemy.orm import relationship
-from os import getenv
+from models.base_model import BaseModel
 
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
-    __tablename__ = "cities"
-    
-    name = Column(
-        String(128),
-        nullable=False
-    )
-
-    state_id = Column(
-        String(60),
-        nullable=False,
-        ForeignKey('states.id'),
-    )
-    
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        places = relationship('Place', backref="cities",
-                              cascade="all, delete, delete-orphan")
+    state_id = ""
+    name = ""
