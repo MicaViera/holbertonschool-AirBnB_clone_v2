165f0e6bd891739453b3794290ef1cab52fc6b29
MicaViera <vieranoelia98@gmail.com>
2023-11-27 01:53:08 +0000

    Restoring state class.

--- models/state.py
+++ models/state.py
@@ -1,31 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-from os import getenv
-from models import storage
+from models.base_model import BaseModel
 
 
 class State(BaseModel):
     """ State class """
-    __tablename__ = "states"
-    
-    name = Column(
-        String(128),
-        nullable=False
-    )
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship('City', 
-            backref="state",
-            cascade="all, delete, delete-orphan")
-
-    @property
-    def cities(self):
-        """Method that returns the list of City instances."""
-        city_list = []
-        for key, value in storage._FileStorage__objects.items():
-            key = key.split(".")
-            if key == 'City' and value.state_id == self.id:
-                city_list.append(value)
-        return city_list
+    name = ""
