c8a8b6c65a9e86aefb4fc4971bdb84607999eaf1
MicaViera <vieranoelia98@gmail.com>
2024-03-08 08:39:20 -0800

    State model

--- models/state.py
+++ models/state.py
@@ -1,8 +1,31 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from os import getenv
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """class State"""
+    __tablename__ = 'states'
+
+    name = Column(
+        String(128),
+        nullable=False)
+
+    data = getenv("HBNB_TYPE_STORAGE")
+
+    if (data == "db"):
+        cities = relationship('City', backref="state", cascade="all, delete")
+
+    if data != 'db':
+        @property
+        def cities(self):
+            """Method that returns a list of cities depends on the given id."""
+            cities = []
+            all_cities = models.storage.all(City)
+            for city_obj in all_cities.values():
+                if city_obj.state_id == self.id:
+                    cities.append(city_obj)
+            return cities
