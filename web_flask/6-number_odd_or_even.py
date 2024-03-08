30a8dc772b7c077311752b6bf5ee0c4924d1b922
MicaViera <vieranoelia98@gmail.com>
2024-03-08 05:17:03 -0800

    Script that starts a Flask web application based on Even or Odd number.

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -51,7 +51,7 @@ def EvenOdd_template(n):
         EvenOdd = "even"
     else:
         EvenOdd = "odd"
-    return render_template('6-number.html', n=n, EvenOdd=EvenOdd)
+    return render_template('6-number_odd_or_even.html', n=n, EvenOdd=EvenOdd)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
